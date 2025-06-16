4511e6cc29e864647b1bfd0641002f5d6901630d
adriannic <nicolas.aguilera.adrian@gmail.com>
2025-06-17 01:51:50 +0200

    feat: remove useless plugins

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -44,48 +44,39 @@ return {
   },
 
   {
-    "folke/flash.nvim",
-    event = "VeryLazy",
-    dependencies = {
+    "phaazon/hop.nvim",
+    opts = {},
+    keys = {
       {
-        "AstroNvim/astrocore",
-        opts = {
-          mappings = {
-            x = {
-              ["s"] = {
-                function() require("flash").jump() end,
-                desc = "Flash",
-              },
-              ["R"] = {
-                function() require("flash").treesitter_search() end,
-                desc = "Treesitter Search",
-              },
-            },
-            o = {
-              ["r"] = {
-                function() require("flash").remote() end,
-                desc = "Remote Flash",
-              },
-              ["R"] = {
-                function() require("flash").treesitter_search() end,
-                desc = "Treesitter Search",
-              },
-              ["s"] = {
-                function() require("flash").jump() end,
-                desc = "Flash",
-              },
-            },
-            n = {
-              ["s"] = {
-                function() require("flash").jump() end,
-                desc = "Flash",
-              },
-            },
-          },
-        },
+        "<leader>j",
+        desc = "Jump",
+        mode = { "n", "v" },
+      },
+      {
+        "<leader>jw",
+        function() require("hop").hint_words() end,
+        mode = { "n" },
+        desc = "Hop hint words",
+      },
+      {
+        "<leader>jl",
+        function() require("hop").hint_lines() end,
+        mode = { "n" },
+        desc = "Hop hint lines",
+      },
+      {
+        "<leader>jw",
+        function() require("hop").hint_words { extend_visual = true } end,
+        mode = { "v" },
+        desc = "Hop hint words",
+      },
+      {
+        "<leader>jl",
+        function() require("hop").hint_lines { extend_visual = true } end,
+        mode = { "v" },
+        desc = "Hop hint lines",
       },
     },
-    opts = {},
   },
 
   -- You can disable default plugins as follows:
