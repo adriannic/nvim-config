6e2a895cb02114e8031abea095458be64f762d81
adriannic <nicolas.aguilera.adrian@gmail.com>
2025-08-11 01:08:51 +0200

    feat: save last file position

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -5,3 +5,10 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.highlight.on_yank()
   end,
 })
+
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  desc = "Return to the last position in a file",
+  group = vim.api.nvim_create_augroup("config.recover-file-pos", { clear = true }),
+  pattern = "*",
+  command = 'silent! normal! g`"zv',
+})
