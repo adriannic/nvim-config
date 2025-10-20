37e1b5cf666b1d304eb1e84b2e47113d5037fdc1
adriannic <nicolas.aguilera.adrian@gmail.com>
2025-10-21 01:51:25 +0200

    feat: enable lsp keymaps always

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -14,3 +14,24 @@ vim.keymap.set("n", "<Leader>c", "<cmd>bdelete<cr>", { desc = "Close buffer" })
 vim.keymap.set("n", "<Leader>/", "gcc<esc>", { desc = "Comment line", remap = true })
 vim.keymap.set("v", "<Leader>/", "gcc<esc>", { desc = "Comment lines", remap = true })
 vim.keymap.set("t", "<ESC>", "<C-\\><C-n>", { noremap = true })
+
+local map = function(keys, func, desc, mode)
+  mode = mode or "n"
+  vim.keymap.set(mode, keys, func, { desc = "LSP " .. desc })
+end
+
+map("<Leader>lD", function() Snacks.picker.diagnostics() end, "find diagnostics")
+map("<Leader>lS", function() require("aerial").toggle() end, "show Symbols")
+map("<Leader>la", vim.lsp.buf.code_action, "apply quickfix", { "n", "x" })
+map("<Leader>la", vim.lsp.buf.code_action, "code Action", { "n", "x" })
+map("<Leader>ld", function() vim.diagnostic.open_float() end, "show hover diagnostics")
+map("<Leader>lf", vim.lsp.buf.format, "format buffer")
+map("<Leader>ll", vim.lsp.codelens.run, "codelens run")
+map("<Leader>lr", vim.lsp.buf.rename, "rename")
+map("<Leader>ls", function() Snacks.picker.lsp_symbols() end, "find Symbols")
+map("gD", function() Snacks.picker.lsp_declarations() end, "goto declaration")
+map("gI", function() Snacks.picker.lsp_implementations() end, "goto implementation")
+map("gd", function() Snacks.picker.lsp_definitions() end, "goto definition")
+map("gr", function() Snacks.picker.lsp_references() end, "goto references")
+map("gt", function() Snacks.picker.lsp_type_definitions() end, "goto type definition")
+
