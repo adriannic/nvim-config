2321b29ea83ec19605f79b999ad1286f39054cd4
adriannic <nicolas.aguilera.adrian@gmail.com>
2026-01-01 02:47:37 +0100

    fix: remove `codesnap` due to conflict with `blink`

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -1,108 +1,133 @@
 return {
-  "saghen/blink.cmp",
-  version = "^1",
-  event = { "InsertEnter", "CmdlineEnter" },
-  opts_extend = {
-    "sources.default",
-    "cmdline.sources",
-    "term.sources",
-  },
-  dependencies = {
-    "rafamadriz/friendly-snippets",
-    "folke/lazydev.nvim",
-    "erooke/blink-cmp-latex",
-    "onsails/lspkind.nvim",
-    "nvim-tree/nvim-web-devicons",
-  },
-  opts = {
-    keymap = {
-      preset = "none",
-      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-      ["<C-e>"] = { "hide", "fallback" },
-      ["<CR>"] = { "accept", "fallback" },
-      ["<C-k>"] = { "select_prev", "fallback" },
-      ["<C-j>"] = { "select_next", "fallback" },
-    },
-    completion = {
-      ghost_text = {
-        enabled = true,
-      },
-      accept = {
-        auto_brackets = {
-          enabled = true,
-        },
-      },
-      documentation = {
-        auto_show = true,
-        auto_show_delay_ms = 0,
-      },
-      menu = {
-        draw = {
-          components = {
-            kind_icon = {
-              text = function(ctx)
-                local icon = ctx.kind_icon
-                if vim.tbl_contains({ "Path" }, ctx.source_name) then
-                  local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
-                  if dev_icon then
-                    icon = dev_icon
-                  end
-                else
-                  icon = require("lspkind").symbolic(ctx.kind, {
-                    mode = "symbol",
-                  })
-                end
+	"saghen/blink.cmp",
+	version = "1.*",
+	event = { "InsertEnter", "CmdlineEnter" },
+	dependencies = {
+		"rafamadriz/friendly-snippets",
+		"onsails/lspkind.nvim",
+		"nvim-tree/nvim-web-devicons",
+		{
+			"folke/lazydev.nvim",
+			ft = "lua",
+			opts = { library = { { path = "${3rd}/luv/library", words = { "vim%.uv" } } } },
+		},
+	},
+	opts = {
+		keymap = {
+			preset = "none",
+			["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+			["<C-e>"] = { "hide", "fallback" },
+			["<CR>"] = { "accept", "fallback" },
+			["<C-k>"] = { "select_prev", "fallback" },
+			["<C-j>"] = { "select_next", "fallback" },
+		},
+		completion = {
+			ghost_text = { enabled = true },
+			accept = { auto_brackets = { enabled = true } },
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 0,
+			},
+			menu = {
+				direction_priority = function()
+					local blink = require("blink.cmp")
+					local ctx = blink.get_context()
+					local item = blink.get_selected_item()
 
-                return icon .. ctx.icon_gap
-              end,
+					if ctx == nil or item == nil then
+						return { "s", "n" }
+					end
 
-              -- Optionally, use the highlight groups from nvim-web-devicons
-              -- You can also add the same function for `kind.highlight` if you want to
-              -- keep the highlight groups in sync with the icons.
-              highlight = function(ctx)
-                local hl = ctx.kind_hl
-                if vim.tbl_contains({ "Path" }, ctx.source_name) then
-                  local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
-                  if dev_icon then
-                    hl = dev_hl
-                  end
-                end
-                return hl
-              end,
-            }
-          }
-        }
-      }
-    },
-    sources = {
-      default = {
-        "lsp",
-        "buffer",
-        "snippets",
-        "path",
-        "lazydev",
-        "latex",
-      },
-      providers = {
-        lazydev = {
-          name = "LazyDev",
-          module = "lazydev.integrations.blink",
-          score_offset = 100,
-        },
-        latex = {
-          name = "Latex",
-          module = "blink-cmp-latex",
-          opts = {
-            insert_command = false,
-          },
-        },
-      },
-    },
-    signature = {
-      enabled = true,
-    },
-    fuzzy = {
-      implementation = "prefer_rust_with_warning",
-    },
-  },
+					local item_text = item.textEdit ~= nil and item.textEdit.newText or item.insertText or item.label
+					local is_multi_line = item_text:find("\n") ~= nil
+
+					if is_multi_line or vim.g.blink_cmp_upwards_ctx_id == ctx.id then
+						vim.g.blink_cmp_upwards_ctx_id = ctx.id
+						return { "n", "s" }
+					end
+
+					return { "s", "n" }
+				end,
+				draw = {
+					components = {
+						kind_icon = {
+							text = function(ctx)
+								local icon = ctx.kind_icon
+								if vim.tbl_contains({ "Path" }, ctx.source_name) then
+									local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
+									if dev_icon then
+										icon = dev_icon
+									end
+								else
+									icon = require("lspkind").symbolic(ctx.kind, {
+										mode = "symbol",
+									})
+								end
+
+								return icon .. ctx.icon_gap
+							end,
+							highlight = function(ctx)
+								local hl = ctx.kind_hl
+								if vim.tbl_contains({ "Path" }, ctx.source_name) then
+									local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
+									if dev_icon then
+										hl = dev_hl
+									end
+								end
+								return hl
+							end,
+						},
+					},
+				},
+			},
+		},
+		sources = {
+			default = {
+				"lsp",
+				"buffer",
+				"snippets",
+				"path",
+			},
+			per_filetype = { lua = { "lazydev" } },
+			providers = {
+				path = {
+					opts = {
+						get_cwd = function()
+							return vim.fn.getcwd()
+						end,
+					},
+				},
+				lsp = {
+					name = "LSP",
+					module = "blink.cmp.sources.lsp",
+					transform_items = function(_, items)
+						return vim.tbl_filter(function(item)
+							return item.kind ~= require("blink.cmp.types").CompletionItemKind.Keyword
+						end, items)
+					end,
+				},
+				lazydev = {
+					name = "LazyDev",
+					module = "lazydev.integrations.blink",
+					score_offset = 100,
+				},
+			},
+		},
+		signature = {
+			enabled = true,
+		},
+		fuzzy = {
+			sorts = {
+				"exact",
+				"score",
+				"sort_text",
+			},
+			implementation = "prefer_rust_with_warning",
+		},
+	},
+	opts_extend = {
+		"sources.default",
+		"cmdline.sources",
+		"term.sources",
+	},
 }
